90e3f2468e5ef3a2eaffc49d5197e335aec35fe2
timi-smiles <timioguns16@gmail.com>
2024-01-26 02:03:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const gradientBox = document.querySelector('.gradient-box');
+const selectMenu = document.querySelector('.select-box select');
+const colorInputs = document.querySelectorAll('.colors input');
+const textarea = document.querySelector('textarea');
+const refreshBtn = document.querySelector('.refresh');
+const copyBtn = document.querySelector('.copy')
+
+const getRandomColor = () =>{
+    //Generating a random color in hexadecimal format.Example #5665E9
+    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${randomHex}`;
+}
+
+const generateGradient = (isRandom) =>{
+    if(isRandom){
+        // console.log(getRandomColor);
+        colorInputs[0].value = getRandomColor();
+        colorInputs[1].value = getRandomColor();
+    }
+    //Creating a gradient string using the color input values
+
+    // console.log('Color update....');
+    const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
+    gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient}`;
+    // console.log(gradient);
+}
+
+const copyCode = () => {
+    //Copying textarea value and updating the copy button text
+    navigator.clipboard.writeText(textarea.value);
+    copyBtn.innerText = 'code Copied!'
+    setTimeout(() => copyBtn.innerText = 'Copy Code', 1600);
+}
+
+colorInputs.forEach(input => {
+    //Calling generateGradient function on each color input clicks
+    input.addEventListener('input', () => generateGradient(false));
+})
+
+selectMenu.addEventListener('change', () => generateGradient(false));
+refreshBtn.addEventListener('click', () => generateGradient(true));
+copyBtn.addEventListener('click', copyCode)
